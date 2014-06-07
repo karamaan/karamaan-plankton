d5d38eb0d774c85431df8edf34bcec5136b6dd25
Tom Ellis <tom-2013@purelyagile.com>
2014-06-07 14:58:42 +0200

    Version 0.3.  Replace lens dependency with lens-family-core.

--- Karamaan/Plankton/Arrow/Lens.hs
+++ Karamaan/Plankton/Arrow/Lens.hs
@@ -1,24 +1,33 @@
-{-# LANGUAGE FlexibleContexts #-}
-
 module Karamaan.Plankton.Arrow.Lens where
 
 import Prelude hiding (id)
 import Control.Arrow (Arrow, arr, (<<<), second)
 import Control.Category (id)
-import Karamaan.Plankton.Arrow.Orphans ()
-import qualified Control.Lens as L
-import qualified Control.Lens.Internal.Context as LC
+import qualified Lens.Family as L
 
-viewA :: Arrow arr => L.Getting a s a -> arr s a
+viewA :: Arrow arr => L.FoldLike a s t a b -> arr s a
 viewA = arr . L.view
 
-overA :: Arrow p => L.LensLike (L.Context a b) s t a b -> p a b -> p s t
+overA :: Arrow p => L.LensLike (Context a b) s t a b -> p a b -> p s t
 overA l p = arr (uncurry id) <<< second p <<< arr go where
-    go s = case l LC.sell s of
-      L.Context f a -> (f, a)
+    go s = case l sell s of
+      Context f a -> (f, a)
 
 -- This is %%~ from Control.Lens but I prefer a name rather than symbols
 -- It isn't to do with /Arrow/ lenses per se, but I put it here rather than
 -- starting a new module just for one function.
-overf :: Functor f => ((a -> f b) -> s -> f t) -> (a -> f b) -> s -> f t
+overf :: Functor f => L.LensLike f s t a b -> (a -> f b) -> s -> f t
 overf = id
+
+-- Context stuff which is from lens.  Since we don't want to import
+-- lens we replicate it here.  Arguably it could be in
+-- lens-family-core.
+
+data Context a b t = Context (b -> t) a
+
+sell :: a -> Context a b b
+sell = Context id
+
+instance Functor (Context a b) where
+  fmap f (Context g t) = Context (f . g) t
+  {-# INLINE fmap #-}
