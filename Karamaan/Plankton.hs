1b51d612a7fea9c11a9fda3c6d45791e7fc8ae87
Tom Ellis <tom-2013@purelyagile.com>
2014-08-20 16:31:00 -0400

    Added some compositions that will be needed for Opaleye

--- Karamaan/Plankton.hs
+++ Karamaan/Plankton.hs
@@ -13,6 +13,13 @@ infix 8 .:
 (.:) :: (i -> r) -> (a1 -> a2 -> i) -> (a1 -> a2 -> r)
 (f .: g) x y = f (g x y)
 
+(.:.) :: (r -> z) -> (a -> b -> c -> r) -> (a -> b -> c -> z)
+(f .:. g) x y z = f (g x y z)
+
+(.:::.) :: (d -> e) -> (a -> a1 -> a2 -> a3 -> a4 -> b -> c -> d)
+        -> a -> a1 -> a2 -> a3 -> a4 -> b -> c -> e
+(f .:::. g) t u v w x y z = f (g t u v w x y z)
+
 zipWithLongest :: a -> b -> (a -> b -> c) -> [a] -> [b] -> [c]
 zipWithLongest xdef ydef f = zip' where
   zip' [] [] = []
